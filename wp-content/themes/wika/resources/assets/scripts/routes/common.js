5eaaacfa6a00603d5255ffd23107aa83d4d9da21
Marcial Soto <marcialsotosanchez@gmail.com>
2018-08-27 09:39:36 -0500

    FAQS terminado

--- wp-content/themes/wika/resources/assets/scripts/routes/common.js
+++ wp-content/themes/wika/resources/assets/scripts/routes/common.js
@@ -1,13 +1,13 @@
 import { library, dom, config } from '@fortawesome/fontawesome-svg-core';
-import { faBackspace, faCircleNotch, faSpinner, faSync, faArrowLeft, faArrowRight, faExclamationTriangle, faSearch, faQuestion, faSignInAlt, faSignOutAlt, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faBackspace, faCircleNotch, faSpinner, faSync, faArrowLeft, faArrowRight, faExclamationTriangle, faSearch, faQuestion, faSignInAlt, faSignOutAlt, faHome, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { faBell, faBookmark, faCalendarAlt, faClock, faCommentAlt, faEdit, faEnvelope, faEye, faEyeSlash, faFile, faFileAlt, faFileExcel, faFilePdf, faFileWord, faImages, faLifeRing, faLightbulb, faListAlt, faNewspaper, faPlayCircle, faSave, faTrashAlt, faUser  } from '@fortawesome/free-regular-svg-icons';
 // import { fab } from '@fortawesome/free-brands-svg-icons';
 
 export default {
   init() {
     // JavaScript to be fired on all pages
+    library.add( faBackspace, faCircleNotch, faSpinner, faSync, faArrowLeft, faArrowRight, faExclamationTriangle, faSearch, faQuestion, faSignInAlt, faSignOutAlt, faHome, faAngleDown );
     library.add(faBell, faBookmark, faCalendarAlt, faClock, faCommentAlt, faEdit, faEnvelope, faEye, faEyeSlash, faFile, faFileAlt, faFileExcel, faFilePdf, faFileWord, faImages, faLifeRing, faLightbulb, faListAlt, faNewspaper, faPlayCircle, faSave, faTrashAlt, faUser );
-    library.add( faBackspace, faCircleNotch, faSpinner, faSync, faArrowLeft, faArrowRight, faExclamationTriangle, faSearch, faQuestion, faSignInAlt, faSignOutAlt, faHome );
     config.searchPseudoElements = true;
     // Replace any existing <i> tags with <svg> and set up a MutationObserver to
     // continue doing this as the DOM changes.
--- wp-content/themes/wika/resources/views/partials/content-faq.blade.php
+++ wp-content/themes/wika/resources/views/partials/content-faq.blade.php
@@ -1,11 +1,9 @@
 <article @php post_class("card") @endphp>
 
   <header id="heading_{{ get_the_ID() }}" class="card-header">
-    <h2 class="entry-title">
-      <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse_{{ get_the_ID() }}" aria-expanded="false" aria-controls="collapse_{{ get_the_ID() }}">
-        {{ get_the_title() }}
-      </button>
-    </h2>
+    <button class="btn btn-block btn-link entry-title" type="button" data-toggle="collapse" data-target="#collapse_{{ get_the_ID() }}" aria-expanded="false" aria-controls="collapse_{{ get_the_ID() }}">
+      {{ get_the_title() }}
+    </button>
   </header>
 
   <div id="collapse_{{ get_the_ID() }}" class="entry-summary collapse" aria-labelledby="heading_{{ get_the_ID() }}" data-parent="#accordionSistemas">
